7ddff7ce4d103451cd8acd3c967fee2ba080029d
Zippocat <Zipocat@gmail.com>
2017-04-04 13:16:44 +0300

    Added admin categories, admin statistics, admin add_product

--- shop.loc/js/index.js
+++ shop.loc/js/index.js
@@ -28,11 +28,36 @@ myApp.controller('myController', ['$scope', '$http', '$location', function ($sco
 	$http.get("./js/orders.json").then(function (response) {
 		$scope.orders = response.data
 	}, error)
+
+	$http.get("./js/categories.json").then(function (response) {
+		$scope.categories = response.data
+	}, error)
 }])
 
 myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 	$locationProvider.hashPrefix('')
 	$routeProvider
+		.when('/top_10_products', {
+			templateUrl: './pages/statistics/top_10_products.html',
+		})
+		.when('/top_customers', {
+			templateUrl: './pages/statistics/top_customers.html',
+		})
+		.when('/earnings', {
+			templateUrl: './pages/statistics/earnings.html',
+		})
+		.when('/statistics', {
+			templateUrl: './pages/statistics/statistics.html',
+		})
+		.when('/add_product', {
+			templateUrl: './pages/add_product/add_product.html',
+		})
+		.when('/edit_categories', {
+			templateUrl: './pages/categories/edit_categories.html',
+		})
+		.when('/create_categories', {
+			templateUrl: './pages/categories/edit_categories.html',
+		})
 		.when('/aordershistory', {
 			templateUrl: './pages/aordershistory/aordershistory.html',
 		})
